fa3cb87a2f6e09afb54576f42791efa4349300ca
César Mayta <cesarmayta@gmail.com>
2023-02-20 21:42:01 -0500

    dia1 monedas

--- SEMANA01/dia1/monedas.py
+++ SEMANA01/dia1/monedas.py
@@ -1,4 +1,7 @@
 opcion = 0
+valorCompra = 3.839
+valorVenta = 3.849
+
 while(opcion != 3):
     print("="*60)
     print(" "* 10 + "CONVERTIDOR DE MONEDAS VERSION 1.0")
@@ -11,11 +14,24 @@ while(opcion != 3):
     print("="*60)
     opcion = int(input("Ingrese una opción del menú:"))
     if(opcion == 1):
+        print("="*60)
         print("CONVERTIR DE SOLES A DOLARES")
+        print("="*60)
+        montoOrigen = input("Ingrese monto en soles : ")
+        montoDestino = float(montoOrigen) / valorVenta
+        monedaDestino = "dolares"
     elif(opcion == 2):
+        print("="*60)
         print("CONVERTIR DE DOLARES A SOLES")
+        print("="*60)
+        montoOrigen = float(input("Ingrese monto en dolares:"))
+        montoDestino = montoOrigen * valorCompra
+        monedaDestino = "soles"
     elif(opcion == 3):
         print("ADIOS !!!")
     else:
         print("debe ingresar una opción valida ...")
+        
+    if(opcion == 1 or opcion == 2):
+        print("El monto en " + monedaDestino + " es " + str(montoDestino))
         
