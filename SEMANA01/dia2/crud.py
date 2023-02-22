6f2913723e98c70a475412e7f677d844b6be84be
César Mayta <cesarmayta@gmail.com>
2023-02-21 22:33:46 -0500

    dia 2 crud actualizar

--- SEMANA01/dia2/crud.py
+++ SEMANA01/dia2/crud.py
@@ -4,12 +4,12 @@ import time
 SISTEMA DE MATRICULA DE ALUMNOS
 C = CREATE | R = READ | U = UPDATE | D = DELETE
 """
-alumno = {
+"""alumno = {
     'nombre':'César Mayta',
     'email':'cesarmayta@gmail.com',
     'celular':'99332233'
-}
-
+}"""
+alumno = {}
 listaAlumnos = [alumno]
 
 ANCHO = 50
@@ -33,10 +33,53 @@ while(opcion != "5"):
     os.system("clear")
     if(opcion == "1"):
         print("[1] REGISTRO DE NUEVO ALUMNO")
+        nombre = input("NOMBRE : ")
+        email = input("EMAIL : ")
+        celular = input("CELULAR : ")
+        dicNuevoAlumno = {
+            'nombre':nombre,
+            'email':email,
+            'celular':celular
+        }
+        listaAlumnos.append(dicNuevoAlumno)
     elif(opcion == "2"):
         print("[2] RELACIÓN DE ALUMNOS")
+        for dicAlumno in listaAlumnos:
+            print('*'*ANCHO)
+            for clave,valor in dicAlumno.items():
+                print(clave + " : " + valor)
+        input("PRESIONE ENTER PARA CONTINUAR...")
     elif(opcion == "3"):
         print("[3] ACTUALIZACIÓN DE ALUMNO")
+        valorBusqueda = input('INGRESE EL EMAIL DEL ALUMO A ACTUALIZAR : ')
+        posicionBusqueda = -1
+        for posicion in range(len(listaAlumnos)):
+            dicAlumno = listaAlumnos[posicion]
+            for clave,valor in dicAlumno.items():
+                if(clave == "email" and valor == valorBusqueda):
+                    posicionBusqueda = posicion
+                    break
+                
+        if (posicionBusqueda == -1):
+            print("NO SE ENCONTRO EL ALUMNO SOLICITADO")
+        else:
+            print("""ALUMNO ENCONTRADO : 
+                  """ + listaAlumnos[posicionBusqueda].get("nombre") + """ -
+                  """ + listaAlumnos[posicionBusqueda].get("email") + """ - 
+                  """ + listaAlumnos[posicionBusqueda].get("celular"))
+            
+            nombre = input('NUEVO NOMBRE: ')
+            email = input('NUEVO EMAIL: ')
+            celular = input('NUEVO CELULAR: ')
+            
+            dicAlumnoEditar = {
+                'nombre':nombre,
+                'email':email,
+                'celular':celular
+            }
+            listaAlumnos[posicionBusqueda] = dicAlumnoEditar
+            print('ALUMNO ACTUALIZADO !!!')
+            
     elif(opcion == "4"):
         print("[4] ELIMINACIÓN DE ALUMNO")
     elif(opcion == "5"):
