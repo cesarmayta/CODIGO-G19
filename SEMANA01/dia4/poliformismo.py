32feb739ddd8ca42d6a5a32bac31ce93ad31bbcd
César Mayta <cesarmayta@gmail.com>
2023-02-23 20:48:15 -0500

    dia4 herencia y polimorfismo

--- SEMANA01/dia4/poliformismo.py
+++ SEMANA01/dia4/poliformismo.py
@@ -3,7 +3,7 @@ class Persona:
         self.nombre = nom
         self.email = ema
         
-    def mostrarDatosPersonales(self):
+    def mostrar(self):
         print("Nombre : " + self.nombre)
         print("Email : " + self.email)
     
@@ -14,16 +14,23 @@ class Alumno(Persona):
         self.nota = nota
     
     def mostrar(self):
-        print("Nombre : " + self.nombre)
-        print("Email : " + self.email)
+        print("*" * 20 + "DATOS DEL ALUMNO")
+        super().mostrar()
         print("Nota : " + str(self.nota))
         
 class Profesor(Persona):
-    pass
+    
+    def __init__(self,nom,ema,esp):
+        super().__init__(nom,ema)
+        self.especialidad = esp
+        
+    def mostrar(self):
+        print("*" * 20 + "DATOS DEL PROFESOR")
+        super().mostrar()
+        print("Especialidad :" + self.especialidad)
         
 alumno1 = Alumno('Carlos tello','ctello@gmail.com',11)
-alumno1.mostrarDatosPersonales()
 alumno1.mostrar()
 
-profesor1 = Profesor('César Mayta','cesarmayta@gmail.com')
-profesor1.mostrarDatosPersonales()
+profesor1 = Profesor('César Mayta','cesarmayta@gmail.com','Backend')
+profesor1.mostrar()
