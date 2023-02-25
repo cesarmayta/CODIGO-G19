264e9f947837ab8758a83bf62782d7889c47141a
César Mayta <cesarmayta@gmail.com>
2023-02-24 20:14:50 -0500

    dia5 bolita con movimiento

--- SEMANA01/dia5/videojuego/main.py
+++ SEMANA01/dia5/videojuego/main.py
@@ -3,6 +3,7 @@ import sys
 
 ANCHO = 640
 ALTO = 480
+FONDO = (0,0,64)
 
 #### CREAMOS OBJETOS DEL VIDEJUEGO
 class Bolita(pygame.sprite.Sprite):
@@ -16,22 +17,41 @@ class Bolita(pygame.sprite.Sprite):
         #para cemtrarlo en la pantalla
         self.rect.centerx = ANCHO / 2
         self.rect.centery = ALTO / 2
+        self.speed = [3,3]
+        
+    def update(self):
+        #verificar si la bolita se sale de la pantalla
+        if self.rect.bottom >= ALTO or self.rect.top <= 0:
+            self.speed[1] = -self.speed[1]
+        elif self.rect.right >= ANCHO or self.rect.left <= 0:
+            self.speed[0] = -self.speed[0]
+        self.rect.move_ip(self.speed)
 
 
 pantalla = pygame.display.set_mode((ANCHO,ALTO))
 
 pygame.display.set_caption('Juego en python de codigo g19')
 
+#crear un reloj para hacer que la bolita se mueva mas lento
+reloj = pygame.time.Clock()
+
 #CREAMOS LOS OBJETOS DEL VIDEOJUEGO
 bolita = Bolita()
 
 while True:
+    #establacer el tiempo del reloj
+    reloj.tick(60)
     #bucle para recorrer los eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             #cerrar videojuego
             sys.exit()
     
+    #movemos la bolita
+    bolita.update()
+    
+    #Pintamos la pantalla
+    pantalla.fill(FONDO)
     #Dibujar la bolita en la pantalla
     pantalla.blit(bolita.image,bolita.rect)
     #actualizar los elementos en la pantalla
