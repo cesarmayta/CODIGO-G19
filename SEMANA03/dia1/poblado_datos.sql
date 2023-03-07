7f49940d567b8e38d60aa24fbfc18ee99a3d3289
César Mayta <cesarmayta@gmail.com>
2023-03-06 22:51:16 -0500

    dia1 poblado final

--- SEMANA03/dia1/poblado_datos.sql
+++ SEMANA03/dia1/poblado_datos.sql
@@ -28,3 +28,22 @@ select * from tbl_matricula;
 insert into tbl_matricula_curso(matricula_id,curso_id)
 values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),
 (2,1),(2,2),(2,3),(2,4),(2,5),(2,6);
+
+--probamos la consulta inicial
+select a.alumno_nombre as nombre,a.alumno_email as email,a.alumno_celular as celular,
+b.bootcamp_nombre,
+CASE
+    WHEN count(c1.curso_nombre) >= 1 THEN "HTML Y CSS"
+    ELSE ""
+END as Curso1,
+CASE
+    WHEN count(c2.curso_nombre) >= 1 THEN "JAVASCRIPT"
+    ELSE ""
+END as Curso2
+from tbl_alumno a
+INNER JOIN tbl_matricula m ON m.alumno_id = a.alumno_id
+INNER JOIN tbl_bootcamp b ON m.bootcamp_id = b.bootcamp_id
+INNER JOIN tbl_matricula_curso mc on mc.matricula_id = m.matricula_id
+LEFT JOIN tbl_curso c1 ON mc.curso_id = c1.curso_id and c1.curso_id = 1
+LEFT JOIN tbl_curso c2 ON mc.curso_id = c2.curso_id and c2.curso_id = 2
+GROUP BY a.alumno_nombre,a.alumno_email,a.alumno_celular,b.bootcamp_nombre
