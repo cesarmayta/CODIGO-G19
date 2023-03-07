6202086a53bda524f85f195fd437b5eb8a0b1296
César Mayta <cesarmayta@gmail.com>
2023-03-06 22:34:05 -0500

    dia1 poblado matriculas

--- SEMANA03/dia1/poblado_datos.sql
+++ SEMANA03/dia1/poblado_datos.sql
@@ -24,5 +24,7 @@ select * from tbl_alumno;
 insert into tbl_matricula(matricula_grupo,alumno_id,bootcamp_id)
 values ('CODIGO G19',1,1),('CODIGO G19',2,1);
 select * from tbl_matricula;
-
 --matricula_curso
+insert into tbl_matricula_curso(matricula_id,curso_id)
+values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),
+(2,1),(2,2),(2,3),(2,4),(2,5),(2,6);
