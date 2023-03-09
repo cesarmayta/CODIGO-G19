440ddc5b0086193b18a1be2501d56131776d6149
César Mayta <cesarmayta@gmail.com>
2023-03-08 22:02:31 -0500

    dia3 template admin

--- SEMANA03/dia3/app/admin/views.py
+++ SEMANA03/dia3/app/admin/views.py
@@ -5,3 +5,23 @@ from . import admin
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
+
+@admin.route('/categoria')
+def categoria():
+    return render_template('admin/categoria.html')
+
+@admin.route('/modalidad')
+def modalidad():
+    return render_template('admin/modalidad.html')
+
+@admin.route('/jornada')
+def jornada():
+    return render_template('admin/jornada.html')
+
+@admin.route('/empresa')
+def empresa():
+    return render_template('admin/empresa.html')
+
+@admin.route('/oferta')
+def oferta():
+    return render_template('admin/oferta.html')
