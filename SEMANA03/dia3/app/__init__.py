ec61404577415da8d6730dfc9c0706febb432e92
César Mayta <cesarmayta@gmail.com>
2023-03-08 21:13:21 -0500

    dia3 bluprints

--- SEMANA03/dia3/app/__init__.py
+++ SEMANA03/dia3/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .portal import portal
+from .admin import admin
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(portal)
+    app.register_blueprint(admin)
     
     return app
