45e0bf7fc9fcd36010017c005e3d65a516ff2dab
César Mayta <cesarmayta@gmail.com>
2023-03-14 21:55:02 -0500

    dia2 api rest completo

--- SEMANA04/dia2/backend/app/blueprints/bp_api/resources.py
+++ SEMANA04/dia2/backend/app/blueprints/bp_api/resources.py
@@ -38,7 +38,60 @@ class CategoriaResource(Resource):
         }
         
         return context
-
+    
+    def post(self):
+        data = request.get_json()
+        descripcion = data['descripcion']
+        categoria = Categoria(descripcion)
+        categoria.save()
+        data_schema = CategoriaSchema()
+        
+        context = {
+            'status':True,
+            'content':data_schema.dump(categoria)
+        }
+        
+        return context
+    
+    def put(self,id):
+        data = request.get_json()
+        categoria = Categoria.get_by_id(id)
+        categoria.categoria_descripcion = data['descripcion']
+        categoria.save()
+        
+        data_schema = CategoriaSchema()
+        context = {
+            'status':True,
+            'content':data_schema.dump(categoria)
+        }
+        
+        return context
+    
+    def delete(self,id):
+        categoria = Categoria.get_by_id(id)
+        categoria.delete()
+        data_schema = CategoriaSchema()
+        context = {
+            'status':True,
+            'content':data_schema.dump(categoria)
+        }
+        
+        return context
+    
+class CatIdResource(Resource):
+    
+    def get(self,id):
+        data = Categoria.get_by_id(id)
+        data_schema = CategoriaSchema()
+        
+        context = {
+            'status':True,
+            'content':data_schema.dump(data)
+        }
+        
+        return context
 
 api.add_resource(IndexResource,'/')
 api.add_resource(CategoriaResource,'/categoria')
+api.add_resource(CategoriaResource,'/categoria/<id>',endpoint='categoria')
+api.add_resource(CategoriaResource,'/catbyid/<id>',endpoint='catbyid')
