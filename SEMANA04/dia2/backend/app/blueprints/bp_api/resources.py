960c3c9357df42cd57c77df3533a17c08c78b1b8
César Mayta <cesarmayta@gmail.com>
2023-03-14 20:41:25 -0500

    dia2 categoria get

--- SEMANA04/dia2/backend/app/blueprints/bp_api/resources.py
+++ SEMANA04/dia2/backend/app/blueprints/bp_api/resources.py
@@ -2,6 +2,9 @@ from flask_restful import Resource,Api
 from flask import request
 from . import bp_api
 
+from .models import Categoria
+from .schemas import CategoriaSchema
+
 api = Api(bp_api)
 
 class IndexResource(Resource):
@@ -23,4 +26,19 @@ class IndexResource(Resource):
     def delete(self):
         pass
 
+class CategoriaResource(Resource):
+    
+    def get(self):
+        data = Categoria.get_all()
+        data_schema = CategoriaSchema(many=True)
+        
+        context = {
+            'status':True,
+            'content':data_schema.dump(data)
+        }
+        
+        return context
+
+
 api.add_resource(IndexResource,'/')
+api.add_resource(CategoriaResource,'/categoria')
--- SEMANA04/dia2/backend/app/blueprints/bp_api/schemas.py
+++ SEMANA04/dia2/backend/app/blueprints/bp_api/schemas.py
@@ -1,2 +1,6 @@
 #create your schemas
 from utils.db import ma
+
+class CategoriaSchema(ma.Schema):
+    class Meta:
+        fields = ('categoria_id','categoria_descripcion')
