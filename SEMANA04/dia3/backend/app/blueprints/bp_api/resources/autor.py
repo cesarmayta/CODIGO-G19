5abb1dfd5f7e1c4d703bf8935f25d4acef337b1f
César Mayta <cesarmayta@gmail.com>
2023-03-15 20:32:49 -0500

    dia4 autor complete

--- SEMANA04/dia3/backend/app/blueprints/bp_api/resources/autor.py
+++ SEMANA04/dia3/backend/app/blueprints/bp_api/resources/autor.py
@@ -10,7 +10,15 @@ api = Api(bp_api)
 class AutorResource(Resource):
     
     def get(self):
-        pass
+        data = Autor.get_all()
+        data_schema = AutorSchema(many=True)
+        
+        context = {
+            'status':True,
+            'content':data_schema.dump(data)
+        }
+        
+        return context
     
     def post(self):
         data = request.get_json()
@@ -29,6 +37,49 @@ class AutorResource(Resource):
         
         return context
     
+    def put(self,id):
+        data = request.get_json()
+        objAutor = Autor.get_by_id(id)
+        objAutor.autor_nombre = data['nombre']
+        objAutor.autor_foto = data['foto']
+        objAutor.autor_descripcion = data['descripcion']
+        objAutor.save()
+        
+        data_schema = AutorSchema()
+        context = {
+            'status':True,
+            'content':data_schema.dump(objAutor)
+        }
+        
+        return context
+    
+    def delete(self,id):
+        objAutor = Autor.get_by_id(id)
+        objAutor.delete()
+        data_schema = AutorSchema()
+        context = {
+            'status':True,
+            'content':data_schema.dump(objAutor)
+        }
+        
+        return context
+    
+class AutorIdResource(Resource):
+    
+    def get(self,id):
+        
+        data = Autor.get_by_id(id)
+        data_schema = AutorSchema()
+        
+        context = {
+            'status':True,
+            'content':data_schema.dump(data)
+        }
+        
+        return context
+    
     
 api.add_resource(AutorResource,'/autor')
+api.add_resource(AutorResource,'/autor/<id>',endpoint='autor')
+api.add_resource(AutorIdResource,'/autorid/<id>',endpoint='autorid')
         
