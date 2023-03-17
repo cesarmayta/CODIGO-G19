b34b6dd1fa3d0c0ef273d876ea5fce087dce38d8
César Mayta <cesarmayta@gmail.com>
2023-03-16 19:32:54 -0500

    dia4

---
+++ SEMANA04/dia4/perudemy/backend/app/blueprints/bp_api/schemas.py
@@ -0,0 +1,21 @@
+#create your schemas
+from utils.db import ma
+
+class CategoriaSchema(ma.Schema):
+    class Meta:
+        fields = ('categoria_id','categoria_descripcion')
+        
+class NivelSchema(ma.Schema):
+    class Meta:
+        fields = ('nivel_id','nivel_descripcion')
+        
+class AutorSchema(ma.Schema):
+    class Meta:
+        fields = ('autor_id','autor_nombre','autor_foto','autor_descripcion')
+        
+class CursoSchema(ma.Schema):
+    class Meta:
+        fields = ('curso_id','curso_titulo','curso_descripcion',
+                  'curso_fecharegistro',
+                  'categoria_id','nivel_id','autor_id',
+                  'curso_imagen','curso_duracion','curso_precio','curso_clases')
