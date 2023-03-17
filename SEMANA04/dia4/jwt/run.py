3fd06f493a7d6f11a82c40a44d3cd740fe88a439
César Mayta <cesarmayta@gmail.com>
2023-03-16 22:55:19 -0500

    dia4 jwt login

--- SEMANA04/dia4/jwt/run.py
+++ SEMANA04/dia4/jwt/run.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask,jsonify,request
 from flask_jwt_extended import (
     create_access_token,get_jwt_identity,
@@ -5,7 +6,8 @@ from flask_jwt_extended import (
 
 app = Flask(__name__)
 
-app.config["JWT_SECRET_KEY"] = "qwerty123"
+app.config["JWT_SECRET_KEY"] = "qwerty12f3"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(minutes=1)
 
 jwt = JWTManager(app)
 
@@ -14,6 +16,22 @@ jwt = JWTManager(app)
 def index():
     context = {
         'content':'ruta publica'
+    }
+    
+    return jsonify(context)
+
+@app.route('/login',methods=["POST"])
+def login():
+    usuario = request.json.get("usuario",None)
+    password = request.json.get("password",None)
+    
+    if usuario == "admin" and password == "1234":
+        token = create_access_token({"usuario":"admin"})
+    else:
+        token = "invalid"
+    
+    context = {
+        'content':token
     }
     
     return jsonify(context)
