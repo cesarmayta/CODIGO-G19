187a4b0a75f4c387d8966cf49f5049fac5567402
César Mayta <cesarmayta@gmail.com>
2023-03-17 20:36:07 -0500

    auth login

--- SEMANA04/dia5/backend/app/blueprints/bp_auth/resources/usuario.py
+++ SEMANA04/dia5/backend/app/blueprints/bp_auth/resources/usuario.py
@@ -15,5 +15,25 @@ class UsuarioResource(Resource):
         }
         
         return context
+    
+    def post(self):
+        username = request.json.get("username",None)
+        password = request.json.get("password",None)
+        
+        payload = {
+            'usuario':username
+        }
+        
+        if username == "admin" and password == "admin":
+            access_token = create_access_token(payload)
+        else:
+            access_token = 'credenciales no validas'
+            
+        context = {
+            'status':True,
+            'content':access_token
+        }
+        
+        return context
 
 api.add_resource(UsuarioResource,'/')
