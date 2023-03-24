d2d5001452887250943290e18102148dc858a8d7
César Mayta <cesarmayta@gmail.com>
2023-03-23 22:35:11 -0500

    modelos

--- SEMANA05/dia4/blog/web/admin.py
+++ SEMANA05/dia4/blog/web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Autor,Articulo,Comentario
+)
+
+admin.site.register(Autor)
+admin.site.register(Articulo)
+admin.site.register(Comentario)
