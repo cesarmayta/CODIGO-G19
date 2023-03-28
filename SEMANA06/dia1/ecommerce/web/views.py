40c2cfcfa67116443b2e709cc45727e2908b12a2
César Mayta <cesarmayta@gmail.com>
2023-03-27 22:33:02 -0500

    dia1 categorias y marcas

--- SEMANA06/dia1/ecommerce/web/views.py
+++ SEMANA06/dia1/ecommerce/web/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import (Categoria,Marca,
+                     Producto,ProductoImagen,
+                     ProductoRelacionado)
+"""
+VISTAS PARA EL CATALOGO DE PRODUCTOS
+"""
 def index(request):
-    return render(request,'index.html')
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    listaProductos = Producto.objects.all()
+    context = {
+        'categorias':listaCategorias,
+        'marcas':listaMarcas,
+        'productos':listaProductos
+    }
+    return render(request,'index.html',context)
