e2d02974b9b4a8b910568cb967ad679e9e218504
César Mayta <cesarmayta@gmail.com>
2023-03-28 19:50:18 -0500

    dia2

---
+++ SEMANA06/dia2/ecommerce-carrito/web/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import (Categoria,Marca,
+                     Producto,ProductoImagen,
+                     ProductoRelacionado)
+"""
+VISTAS PARA EL CATALOGO DE PRODUCTOS
+"""
+def index(request):
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    listaProductos = Producto.objects.all()
+    context = {
+        'categorias':listaCategorias,
+        'marcas':listaMarcas,
+        'productos':listaProductos
+    }
+    return render(request,'index.html',context)
+
+def producto(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+    return render(request,'producto.html',context)
+
+""" CARRITO DE COMPRAS """
+
+def carrito(request):
+    request.session["total"] = 100
+    return render(request,'carrito.html')
