49b0d88c14aa713bf0d5567dc0d6a2f1d15be917
César Mayta <cesarmayta@gmail.com>
2023-04-03 20:22:38 -0500

    dia1 mesa api get

---
+++ SEMANA07/dia1/pos_backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('mesa',views.MesaView.as_view())
+]
--- SEMANA07/dia1/pos_backend/api/views.py
+++ SEMANA07/dia1/pos_backend/api/views.py
@@ -1,3 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import (
+    Mesa,Categoria
+)
+
+from .serializers import (
+    MesaSerializer,
+    CategoriaSerializer
+)
+
+class MesaView(APIView):
+    
+    def get(self,request):
+        data = Mesa.objects.all()
+        serData = MesaSerializer(data,many=True)
+        
+        context = {
+            'ok':True,
+            'content':serData.data
+        }
+        
+        return Response(context)
