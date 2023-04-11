7324616668b78503aea588d561a9d4face519fae
César Mayta <cesarmayta@gmail.com>
2023-04-10 20:13:52 -0500

    dia4 apirest categoria y plato

--- SEMANA07/dia4/pos_backend/api_admin/views.py
+++ SEMANA07/dia4/pos_backend/api_admin/views.py
@@ -1,13 +1,22 @@
 from rest_framework import viewsets
 
 from api.models import (
-    Mesa
+    Mesa,Categoria,Plato
 )
 
 from api.serializers import (
-    MesaSerializer
+    MesaSerializer,CategoriaSerializer,
+    PlatoSerializer
 )
 
 class MesaViewSet(viewsets.ModelViewSet):
     queryset = Mesa.objects.all()
     serializer_class = MesaSerializer
+    
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
+class PlatoViewSet(viewsets.ModelViewSet):
+    queryset = Plato.objects.all()
+    serializer_class = PlatoSerializer
