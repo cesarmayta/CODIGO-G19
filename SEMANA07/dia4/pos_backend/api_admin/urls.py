d33d9ef2c8b35bb4843524ffe29a55eed74ed48a
César Mayta <cesarmayta@gmail.com>
2023-04-10 20:34:43 -0500

    dia4 upload img

--- SEMANA07/dia4/pos_backend/api_admin/urls.py
+++ SEMANA07/dia4/pos_backend/api_admin/urls.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path,include
 
 from . import views
 
@@ -8,4 +9,9 @@ router.register(r'mesa',views.MesaViewSet,basename='mesas')
 router.register(r'categoria',views.CategoriaViewSet,basename='categorias')
 router.register(r'plato',views.PlatoViewSet,basename='platos')
 
-urlpatterns = router.urls
+
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('plato/uploadimg',views.UploadPlatoImagenView.as_view())
+]
--- SEMANA07/dia4/pos_backend/api_admin/views.py
+++ SEMANA07/dia4/pos_backend/api_admin/views.py
@@ -1,5 +1,13 @@
 from rest_framework import viewsets
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser,JSONParser
+
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from api.models import (
     Mesa,Categoria,Plato
 )
@@ -10,13 +18,47 @@ from api.serializers import (
 )
 
 class MesaViewSet(viewsets.ModelViewSet):
+    authentication_classes  = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     queryset = Mesa.objects.all()
     serializer_class = MesaSerializer
     
 class CategoriaViewSet(viewsets.ModelViewSet):
+    authentication_classes  = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
     
 class PlatoViewSet(viewsets.ModelViewSet):
+    authentication_classes  = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     queryset = Plato.objects.all()
     serializer_class = PlatoSerializer
+    
+######## subir archivos a cloudinary
+import cloudinary.uploader
+
+class UploadPlatoImagenView(APIView):
+    authentication_classes  = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    parser_classes = (
+        MultiPartParser,
+        JSONParser
+    )
+    
+    @staticmethod
+    def post(request):
+        file = request.data.get('plato_img')
+        
+        upload_data = cloudinary.uploader.upload(file)
+        
+        context = {
+            'status':True,
+            'content':upload_data
+        }
+        
+        return Response(context,status=201)
