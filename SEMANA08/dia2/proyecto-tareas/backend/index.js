fa9479ad025fd4daa4bd6354f304e16aa3b3a43c
César Mayta <cesarmayta@gmail.com>
2023-04-11 22:51:57 -0500

    dia2 cors

--- SEMANA08/dia2/proyecto-tareas/backend/index.js
+++ SEMANA08/dia2/proyecto-tareas/backend/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const mysqlConnection = require('./database')
 
+cors = require('cors')
+
 const app = express()
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res)=>{
