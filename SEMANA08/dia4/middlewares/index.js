57c684839281ebc63641011070439722a4b32b32
César Mayta <cesarmayta@gmail.com>
2023-04-13 19:33:45 -0500

    dia4 middlewares de aplicacion

--- SEMANA08/dia4/middlewares/index.js
+++ SEMANA08/dia4/middlewares/index.js
@@ -2,6 +2,21 @@ const express = require('express')
 
 const app = express()
 
+//middlewares de aplicación
+app.use(function(req,res,next){
+    console.log('esto es un middleware')
+    next()
+})
+app.use((req,res,next)=>{
+    const timeElapsed = Date.now()
+    const today = new Date(timeElapsed)
+    console.log('ejecutado a las',today.toISOString())
+    next()
+
+})
+
+
+
 app.get('/',(req,res)=>{
     res.json({
         status:true,
