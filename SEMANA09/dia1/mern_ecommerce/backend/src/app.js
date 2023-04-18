1daf9a6edfc98a86af632790e759c59cf1d9d58b
César Mayta <cesarmayta@gmail.com>
2023-04-17 22:22:50 -0500

    dia1 plantilla base

---
+++ SEMANA09/dia1/mern_ecommerce/backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const {config} = require('./config')
+const cors = require('cors')
+
+const app = express()
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+//configuración de puerto
+app.set('port',config.port)
+
+app.get('/',(req,res)=>{
+    res.json({
+        "success":true,
+        "message":"api rest de ecommerce con stack MERN"
+    })
+})
+
+module.exports = app
