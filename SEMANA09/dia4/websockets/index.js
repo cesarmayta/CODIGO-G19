205ff61081ad8fad41f6a3e0cae3cbdb80b08968
César Mayta <cesarmayta@gmail.com>
2023-04-20 22:17:05 -0500

    dia4 cliente websocket

--- SEMANA09/dia4/websockets/index.js
+++ SEMANA09/dia4/websockets/index.js
@@ -7,3 +7,12 @@ const path = require('path')
 app.use(express.static(path.join(__dirname,'public')))
 
 const server = app.listen(port,()=>console.log('http://localhost:5000'))
+
+/************* web socket ***************/
+const SocketIO = require('socket.io')
+
+const io = SocketIO(server)
+
+io.on('connection',(socket)=>{
+    console.log('nueva conexión con id',socket.id)
+})
