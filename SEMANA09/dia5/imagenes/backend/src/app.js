4f25636ce0242343c554c42217eeeb3e5ec20e80
César Mayta <cesarmayta@gmail.com>
2023-04-21 23:01:04 -0500

    dia5 docker compose final

--- SEMANA09/dia5/imagenes/backend/src/app.js
+++ SEMANA09/dia5/imagenes/backend/src/app.js
@@ -21,4 +21,8 @@ app.get('/',(req,res)=>{
     })
 })
 
+app.use('/categories',require('./routes/category.route'))
+app.use('/products',require('./routes/product.route'))
+app.use('/users',require('./routes/user.route'))
+
 module.exports = app
--- SEMANA09/dia5/imagenes/backend/src/config/index.js
+++ SEMANA09/dia5/imagenes/backend/src/config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const config = {
     port: process.env.PORT || '5000',
-    mongoUri : process.env.MONGOURI || 'mongodb://localhost:27017/db_shop_g19',
+    mongoUri : process.env.MONGOURI,
     cloudinary:{
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
